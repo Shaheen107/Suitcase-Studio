2fba3ba74416634ef9bfa82d2d7df5390acd083d
dsv <zxcv>
2024-12-05 18:06:21 +0500

    Initial Commit

---
+++ Suitcase Studio/Suitcase_StudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Suitcase_StudioApp.swift
+//  Suitcase Studio
+//
+//  Created by Muhammad Rizwan on 05/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Suitcase_StudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
